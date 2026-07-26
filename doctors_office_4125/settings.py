1985d79edf360337ff09d9a8c6960b29162b4db1
wenling-chang <chloechangling@gmail.com>
2026-07-25 22:54:03 -0700

    Config for static files on nginx

--- doctors_office_4125/settings.py
+++ doctors_office_4125/settings.py
@@ -133,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # custom usermodel
 AUTH_USER_MODEL = 'accounts.CustomUser'
